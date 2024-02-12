0ccb82907b51b3dd89b66e6c1bc7687b6eb1f160
glega.eth <71074904+cryptoCEU@users.noreply.github.com>
2024-02-12 13:29:41 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default async function Page() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{backgroundImage: "url('https://ipfs.decentralized-content.com/ipfs/QmU5xXfFM5JKwPRnDhPK9BxiiE5N1oJHJfJqxcwcsxBJgw')", backgroundSize: "cover"}}>
       <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
         <div className="flex justify-center items-center bg-black rounded-full w-16 sm:w-24 h-16 sm:h-24 my-8">
-          <img src="https://ipfs.decentralized-content.com/ipfs/QmX18eetXj1QBGZ25mCbqYkkkYLmAZfdbJXiFn31Q2qD7n" alt="CryptoPlaza Logo" className="h-8 sm:h-16 invert p-3 mb-1" />
+          <img src="https://ipfs.decentralized-content.com/ipfs/QmX18eetXj1QBGZ25mCbqYkkkYLmAZfdbJXiFn31Q2qD7n" alt="CryptoPlaza Logo"  />
         </div>
         <h1 className="text-lg sm:text-2xl font-bold mb-2" style={{color: "white"}}>
           Cryptoplaza Farcaster Polls
         </h1>
-        <h2 className="text-md sm:text-xl mx-4">
+        <h2 className="text-md sm:text-xl mx-4" style={{color: "white"}>
           Create a new poll with upto 4 options
         </h2>
         <div className="flex flex-wrap items-center justify-around max-w-4xl my-8 sm:w-full bg-white rounded-md shadow-xl h-full border border-gray-100">
