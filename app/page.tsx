266f1ce5451a44768d70da7241a1e3c8cf6b1afb
glega.eth <71074904+cryptoCEU@users.noreply.github.com>
2024-02-12 13:26:38 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,66 +5,6 @@ export let metadata = {
   description: "Poll creator for farcaster",
 };
 
-function VercelLogo(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      aria-label="Vercel Logo"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 19"
-      {...props}
-    >
-      <path
-        clipRule="evenodd"
-        d="M12.04 2L2.082 18H22L12.04 2z"
-        fill="#000"
-        fillRule="evenodd"
-        stroke="#000"
-        strokeWidth="1.5"
-      />
-    </svg>
-  );
-}
-
-export default async function Page() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{backgroundImage: "url('https://ipfs.decentralized-content.com/ipfs/QmU5xXfFM5JKwPRnDhPK9BxiiE5N1oJHJfJqxcwcsxBJgw')", backgroundSize: "cover"}}>
-      <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
-        <div className="flex justify-center items-center bg-black rounded-full w-16 sm:w-24 h-16 sm:h-24 my-8">
-        </div>
-        <h1 className="text-lg sm:text-2xl font-bold mb-2" style={{color: "white"}>
-          Cryptoplaza Farcaster Polls
-        </h1>
-        <h2 className="text-md sm:text-xl mx-4" style={{color: "white"}>
-          Create a new poll with upto 4 options
-        </h2>
-        <div className="flex flex-wrap items-center justify-around max-w-4xl my-8 sm:w-full bg-white rounded-md shadow-xl h-full border border-gray-100">
-          <PollCreateForm />
-        </div>
-      </main>
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { PollCreateForm } from "./form";
-
-export let metadata = {
-  title: "CryptoPlaza Farcaster polls",
-  description: "Poll creator for farcaster",
-};
-
 export default async function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{backgroundImage: "url('https://ipfs.decentralized-content.com/ipfs/QmU5xXfFM5JKwPRnDhPK9BxiiE5N1oJHJfJqxcwcsxBJgw')", backgroundSize: "cover"}}>
@@ -85,4 +25,3 @@ export default async function Page() {
     </div>
   );
 }
-
